b76953cc0b4edbad12decdfd1ef378d44b797760
Kenneth Palaganas <kenma9123@gmail.com>
2013-09-03 02:38:44 +0800

    GET /form/{id}/reports added to PHP sdk

--- JotForm.php
+++ JotForm.php
@@ -414,6 +414,15 @@ class JotForm {
 
     /**
     * [getFolder Get folder details]
+    * @param  [integer] $formID [Form ID is the numbers you see on a form URL. You can get form IDs when you call /user/forms.]
+    * @return [array] [Returns a list of reports in a form, and other details about the reports such as title.]
+    */
+    public function getFormReports($formID){
+        return $this->_executeGetRequest("form/".$formID."/reports");
+    }
+
+    /**
+    * [getFolder Get folder details]
     * @param  [integer] $folderID [You can get a list of folders from /user/folders.]
     * @return [array] [Returns a list of forms in a folder, and other details about the form such as folder color.]
     */
